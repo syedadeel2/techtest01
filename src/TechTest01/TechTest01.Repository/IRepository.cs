b0909678e657cf5f3495c6b5fc11cccf8f931fa7
Praveen Sewak <praveensewak@gmail.com>
2016-03-02 16:43:11 +1100

    initial commit

---
+++ src/TechTest01/TechTest01.Repository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using TechTest01.Domain;
+
+namespace TechTest01.Repository
+{
+    public interface IRepository<TEntity> where TEntity : BaseEntity
+    {
+        
+    }
+}
