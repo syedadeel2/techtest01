93f78b8eb0bbf6011b0388086347e82d13457acf
Adeel Rizvi <adeel.rizvi@yda.net.au>
2018-02-06 18:36:21 +1100

    AngularController UnitTest Added

---
+++ src/TechTest01/TechTest01.Tests/Web/AngularControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTest01.Domain.Catalog;
+using TechTest01.Repository;
+using TechTest01.Services.Catalog;
+using TechTest01.Web.Controllers;
+
+namespace TechTest01.Tests.Web
+{
+    [TestClass]
+    public class AngularControllerTest
+    {
+        [TestMethod]
+        public void AngularControllerView_Test()
+        {
+
+             var controller = new AngularController();
+            var result = controller.Index() as ViewResult;
+
+            Assert.IsTrue(true);
+
+        }
+    }
+}
